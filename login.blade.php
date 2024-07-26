7c83fe574d3b72955b9763ba23a0ac59006d2959
AnhTr14 <20520137@gm.uit.edu.vn>
2024-07-26 09:57:10 +0700

    Add h4

--- login.blade.php
+++ login.blade.php
@@ -12,7 +12,8 @@
     <div style="display: flex; justify-content: center;">
         <div class="card card-info" style="width: 50%;">
             <div class="card-header">
-                <h3 class="card-title">Mới nè</h3>
+                <h3 class="card-title">Đăng nhập</h3>
+                <h4>Đây là TrucAnh</h4>
             </div>
             <!-- /.card-header -->
             <!-- form start -->
